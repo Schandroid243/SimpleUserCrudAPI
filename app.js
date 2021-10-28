40caf20b795d7f7794f747b4933eebb99b89ee87
Schandroid243 <schadrackngunza@gmail.com>
2021-10-28 09:56:42 +0100

    first commit creating app.js to launch server Added User model for mongoDb Added user routes

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoute = require('./routes/users');
+const cors = require('cors');
+require('dotenv/config');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use('/users', userRoute);
+
+app.get('/', (req, res) => {
+    res.send('Hello this my user server !');
+});
+
+
+mongoose.connect(process.env.DB_CONNECTION, ()=>{
+    console.log('Connected to the database !');
+});
+
+app.listen(9000);
---
+++ routes/users.js
@@ -0,0 +1,71 @@
+const { json } = require('express');
+const express = require('express');
+const User = require('../models/User');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try{
+        const users = await User.find();
+        res.json(users);
+    }catch(err) {
+        res.json({message: err});
+    }
+});
+
+router.get('/:id', async(req, res) => {
+    try{
+        const user = await User.findById(req.params.id);
+        res.json(user);
+    } catch(err) {
+        res.json({message: err});
+    }
+});
+
+router.post('/', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        phonenumber: req.body.phonenumber,
+        password: req.body.password
+    });
+    try{
+        const savedUser = await user.save();
+        res.json(savedUser);
+    }catch(err) {
+        res.json({message: err});
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try{
+        const user = await User.updateOne({
+            _id: req.params.id,
+            $set: {
+                name: req.body.name,
+                username: req.body.username,
+                email: req.body.email,
+                phonenumber: req.body.phonenumber,
+                password: req.body.password
+            }
+        });
+        res.json(user);
+    }catch(err) {
+        res.json({message: err});
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const user = await User.deleteOne({_id: req.params.id});
+        res.json(user);
+    } catch(err) {
+        res.json({message: err});
+    }
+});
+
+
+
+
+module.exports = router;
